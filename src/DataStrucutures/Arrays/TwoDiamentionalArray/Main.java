e4aa33c9504c4f4651bc01b383656e3d455a01d1
Akash kalamulla <akashkalamulla1@gmail.com>
2023-12-28 14:57:43 +0530

    Update

--- src/DataStrucutures/Arrays/TwoDiamentionalArray/Main.java
+++ src/DataStrucutures/Arrays/TwoDiamentionalArray/Main.java
@@ -1,5 +1,21 @@
 package DataStrucutures.Arrays.TwoDiamentionalArray;
-
+import java.util.Arrays;
 public class Main {
-  
+
+  public static void main(String[] args) {
+  //    int[][] int2DArray;
+
+  // int2DArray = new int[2][2];
+
+  // int2DArray[0][0] = 1;
+  // int2DArray[0][1] = 2;
+  // int2DArray[1][0] = 3;
+  // int2DArray[1][1] = 4;
+
+  // System.out.println(Arrays.deepToString(int2DArray));
+
+  // String s2DArray[][] = {{"a","b"},{"c","d"}};
+  // System.out.println(Arrays.deepToString(s2DArray));
+  }
+ 
 }
