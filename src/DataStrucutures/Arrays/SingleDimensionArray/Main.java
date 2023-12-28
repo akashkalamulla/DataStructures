a86bb590da052128f3b29b33a56d83dbacb8ef08
Akash kalamulla <akashkalamulla1@gmail.com>
2023-12-28 14:38:38 +0530

    commit

--- src/DataStrucutures/Arrays/SingleDimensionArray/Main.java
+++ src/DataStrucutures/Arrays/SingleDimensionArray/Main.java
@@ -3,8 +3,25 @@ package DataStrucutures.Arrays.SingleDimensionArray;
 public class Main {
   public static void main(String[] args) {
     SingleDimensionArray sda = new SingleDimensionArray(10);
-    sda.insert(1, 10);
-    sda.insert(5, 50);
-    sda.insert(9, 5000);
+    // sda.insert(0, 23); 
+    // sda.insert(1, 10);
+    //  sda.insert(2, 11);
+    //  sda.insert(3, 25);
+    //  sda.insert(5, 50);
+    //  sda.insert(9, 5000);
+
+    // var firstElemet = sda.arr[0];
+    // System.out.println(firstElemet);
+    // var thirdElement = sda.arr[2];
+    // System.out.println(thirdElement);
+
+    // System.out.println("Array traversal");
+    // sda.traverseArray();
+
+    System.out.println("Array Delete elements");
+    sda.deleteValue(1);
+
+
+    
   }
 }
