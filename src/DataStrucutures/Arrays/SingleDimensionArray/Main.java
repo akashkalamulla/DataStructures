9eaecbbcbee65a4e8b511e3c8dc29951b4ac911a
Akash kalamulla <akashkalamulla1@gmail.com>
2023-12-28 11:08:44 +0530

    Update

---
+++ src/DataStrucutures/Arrays/SingleDimensionArray/Main.java
@@ -0,0 +1,10 @@
+package DataStrucutures.Arrays.SingleDimensionArray;
+
+public class Main {
+  public static void main(String[] args) {
+    SingleDimensionArray sda = new SingleDimensionArray(10);
+    sda.insert(1, 10);
+    sda.insert(5, 50);
+    sda.insert(9, 5000);
+  }
+}
