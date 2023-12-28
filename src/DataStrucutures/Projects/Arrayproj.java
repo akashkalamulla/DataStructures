35b4de4a5e58e799e5478bfa645a2fdc4499c5d2
Akash kalamulla <akashkalamulla1@gmail.com>
2023-12-28 17:23:46 +0530

    add

---
+++ src/DataStrucutures/Projects/Arrayproj.java
@@ -0,0 +1,13 @@
+package DataStrucutures.Projects;
+import java.util.Scanner;
+public class Arrayproj {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("How many days tempreture");
+    int numDays = scanner.nextInt();
+    int[] temps = new int[numDays];
+
+    
+
+  }
+}
