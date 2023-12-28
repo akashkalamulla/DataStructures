7f73351dcf6a85097bf0286dff2b3734facf3755
Akash kalamulla <akashkalamulla1@gmail.com>
2023-12-29 01:28:04 +0530

    add

--- src/DataStrucutures/CodingQuestion/ArrayQuestion/RemoveDuplicatestwo.java
+++ src/DataStrucutures/CodingQuestion/ArrayQuestion/RemoveDuplicatestwo.java
@@ -12,6 +12,20 @@ Output: 2
 */
 public class RemoveDuplicatestwo {
   public int removeDuplicates(int[] nums) {
-    
+    if (nums.length == 0) {
+      return 0;
+  }
+
+  int i = 0;
+
+  for (int j = 1; j < nums.length; j++) {
+      if (nums[j] != nums[i]) {
+          i++;
+          nums[i] = nums[j];
+      }
+  }
+
+  return i + 1;
   }
 }
+
---
+++ src/DataStrucutures/CodingQuestion/ArrayQuestion/TwoSum.java
@@ -0,0 +1,5 @@
+package DataStrucutures.CodingQuestion.ArrayQuestion;
+
+public class TwoSum {
+  
+}
